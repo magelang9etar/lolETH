18718a585330f5eb55448ed772e490289120f316
miningmanja <78244471+miningmanja@users.noreply.github.com>
2021-02-07 08:23:17 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:14444
-WALLET=0x01c0D7f32A2b42ebf4D850c15eF6C22695FeDDD0
+WALLET=0x27cd1fe7c5946611d70f45d0d89c514f4106a08f
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
 
 cd "$(dirname "$0")"
